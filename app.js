0c94d43041d2bb0d8ffaec6c04f4ffda15c1712c
ccarlitoxs <ccarlos_ivan@hotmail.com>
2020-06-11 01:11:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 //Puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -22,7 +22,7 @@ app.use(express.static('assets'));
 app.use('/assets', express.static(__dirname + '/assets'));
  
 //arrancar el servidor
-app.listen(port,'0.0.0.0',() => {
-    console.log(`puerto:${port}`);
+app.listen(PORT,'0.0.0.0',() => {
+    console.log(`puerto:${PORT}`);
 }
 );
