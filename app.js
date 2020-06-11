c58f15f8b2c30c000dc906c3ebaaeeba3ae91f94
ccarlitoxs <ccarlos_ivan@hotmail.com>
2020-06-11 05:30:36 -0300

    Casi terminado falta ipn

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+require('dotenv').config({path:'variables.env'});
  
 var app = express();
  
@@ -10,14 +13,119 @@ app.set('view engine', 'handlebars');
 const PORT = process.env.PORT || 4000;
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home',{page:"home"});
 });
 
+
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+
+    var initPoint;
+
+
+    // Agrega credenciales
+    mercadopago.configure({
+    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+    });
+
+    // Crea un objeto de preferencia
+    let preference = {
+    items: [
+        {
+        id:1234,
+        description: "Dispositivo móvil de Tienda e-commerce",
+        title: req.query.title,
+        picture_url: process.env.BASE_URLSERVER+req.query.img.substr(1),
+        unit_price: parseFloat(req.query.price),
+        quantity: parseFloat(req.query.unit),
+        
+        }
+    ],
+    "payer": {
+        "name": "Lalo",
+        "surname": "Landa",
+        "email": "test_user_63274575@testuser.com",
+        "phone": {
+            "area_code": "11",
+            "number": 22223333
+        },
+        "identification": {
+            "type": "DNI",
+            "number": "12345678"
+        },
+        "address": {
+            "street_name": "False",
+            "street_number": 123,
+            "zip_code": "1111"
+        }
+    },
+    "external_reference": "co.carlosivan@gmail.com",
+    "back_urls": {
+        "success": process.env.BASE_URLSERVER+"/aprobado",
+        "failure": process.env.BASE_URLSERVER+"/rechazado",
+        "pending": process.env.BASE_URLSERVER+"/pendiente"
+    },
+    "auto_return": "approved",
+    "payment_methods": {
+        "excluded_payment_methods": [
+            {
+                "id": "amex"
+            }
+        ],
+        "excluded_payment_types": [
+            {
+                "id": "atm"
+            }
+        ],
+        "installments": 6
+    },
+    // "notification_url":process.env.BASE_URLSERVER+"/ipn"
+    };
+
+    mercadopago.preferences.create(preference)
+    .then(function(response){
+    // Este valor reemplazará el string "$$init_point$$" en tu HTML
+    console.log(response);
+    initPoint = response.body.init_point;
+    res.render('detail', {initPoint:initPoint,page:"item",title:req.query.title,price:req.query.price,unit:req.query.unit,img:req.query.img});
+    }).catch(function(error){
+    console.log(error);
+    res.render('error');
+    });
+    
+});
+
+app.get('/aprobado', function (req, res) {
+
+    res.render('aprobado',{page:"home",respuesta:req.query});
+});
+
+app.get('/pendiente', function (req, res) {
+
+    res.render('pendiente',{page:"home",respuesta:req.query});
+});
+
+app.get('/rechazado', function (req, res) {
+
+    res.render('rechazado',{page:"home",respuesta:req.query});
+});
+
+app.post('/ipn', function (req, res) {
+    console.log(req.body);
+    console.log(req.query);
+    // if (req.body.collection_status === 'approved') {
+        
+    // } else if (req.body.collection_status === 'pending') {
+    //     res.render('pendiente',{page:"home",respuesta:req.query});
+    // } else {
+    //     res.render('rechazado',{page:"home",respuesta:req.query});
+    // }
+
+    res.sendStatus(200);
 });
 
 app.use(express.static('assets'));
+console.log(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
@@ -26,3 +134,15 @@ app.listen(PORT,'0.0.0.0',() => {
     console.log(`puerto:${PORT}`);
 }
 );
+
+// {
+//     collection_id: '7054861764',
+//     collection_status: 'approved',
+//     external_reference: 'co.carlosivan@gmail.com',
+//     payment_type: 'credit_card',
+//     merchant_order_id: '1524560954',
+//     preference_id: '469485398-62f6aba1-44c3-406a-bfd7-08bece5cd07d',
+//     site_id: 'MLA',
+//     processing_mode: 'aggregator',
+//     merchant_account_id: 'null'
+//   }
