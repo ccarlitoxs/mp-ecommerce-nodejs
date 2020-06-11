514943bbc0b5ad9f275a63f32a3af18d0b618960
ccarlitoxs <ccarlos_ivan@hotmail.com>
2020-06-11 15:05:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -119,6 +119,7 @@ app.post('/ipn', function (req, res) {
     console.log('ipn ejecutada');
     console.log(req.query);
     console.log(req);
+    console.log(req.body);
 
     mercadopago.configure({
         access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
